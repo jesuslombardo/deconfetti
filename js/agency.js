08368edbef593311800968861a3f6faaee9c787c
Jesús <jesus@digitalhouse.com>
2017-09-14 17:29:56 -0300

    Scroll up

--- js/agency.js
+++ js/agency.js
@@ -30,9 +30,13 @@
   $(window).scroll(function() {
     if ($("#mainNav").offset().top > 100) {
       $("#mainNav").addClass("navbar-shrink");
+      $('#return-to-top').fadeIn(200);
     } else {
       $("#mainNav").removeClass("navbar-shrink");
+      $('#return-to-top').fadeOut(200);
     }
+
+
   });
 
 })(jQuery); // End of use strict
@@ -59,6 +63,8 @@ $( document ).ready(function() {
     });
 
 });
+
+
 
 function scaleVideoContainer() {
 
@@ -105,3 +111,21 @@ function scaleBannerVideoSize(element){
 
     });
 }
+
+
+
+// ===== Scroll to Top ==== 
+/*
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+        $('#return-to-top').fadeIn(200);    // Fade in the arrow
+    } else {
+        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+    }
+});*/
+
+$('#return-to-top').click(function() {      // When arrow is clicked
+    $('body,html').animate({
+        scrollTop : 0                       // Scroll to top of body
+    }, 500);
+});
